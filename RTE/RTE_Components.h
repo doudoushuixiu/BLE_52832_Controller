b39672247815447be4d26bd5f89b03f6306707f8
Xu Jiawei <jiawei.xu@spidersens.com>
2016-05-17 15:40:46 +0800

    修改工程名

--- RTE/RTE_Components.h
+++ RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'ble_app_hrs_s132_with_dfu_pca10040' 
+ * Project: 'ble_app_controller_s132_with_dfu_pca10040' 
  * Target:  's132_pca10040' 
  */
 
